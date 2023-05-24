b1360946c69d325fb69bb3aef002a07a118f69b7
Yan Skvarada <yan@MacBook-Air-Yan.local>
2023-05-24 22:33:11 +0300

    add

---
+++ src/components/Heder/Heder.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+// import styles from "./Heder.module.scss";
+// import styles from "./Heder.module.scss";
+
+const Heder = () => {
+  //   return <div className={styles.container}></div>;
+};
+
+export default Heder;
